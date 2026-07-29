c9382db8da66acb269b882da7c8990081fafec26
Paul Min <info@paulmin.nl>
2026-07-29 08:06:00 +0200

    chore: update dependencies

---
+++ README.md
@@ -0,0 +1,15 @@
+# component-test
+
+This is een experiment. I am trying to compose a wasm component.
+
+There are two ways to do this:
+
+## using the wac-cli
+```bash
+wac compose compose.wac
+```
+
+## using the rust project
+```bash
+cargo run --release
+```
--- src/main.rs
+++ src/main.rs
@@ -75,6 +75,7 @@ async fn main() -> Result<(), Error> {
             PublishOpts {
                 package: Some((SERVER.package_name().parse()?, SERVER.version.parse()?)),
                 registry: None,
+                ..Default::default()
             },
         )
         .await?;
